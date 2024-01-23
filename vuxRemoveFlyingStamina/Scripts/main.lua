03a7fcc9cccb7067e05c558d26db06d4bb29b964
Chunn241529 <133483726+Chunn241529@users.noreply.github.com>
2024-01-23 13:53:16 +0700

    Initial commit

---
+++ vuxRemoveFlyingStamina/Scripts/main.lua
@@ -0,0 +1,7 @@
+NotifyOnNewObject("/Script/Pal.PalGameSetting", function(var)
+	var.FlyHorizon_SP = 0
+	var.FlyHorizon_Dash_SP = 0
+	var.FlyHover_SP = 0
+	var.FlyMaxHeight = 999999
+	var.FlyVertical_SP = 0
+end)
